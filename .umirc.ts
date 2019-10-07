b3085722ab9f49fb814a3d743fdd24fbd50c4d9e
Arthur Sakhievich Fayzrakhmanov <heraldhoi@gmail.com>
2019-10-08 00:30:12 +0500

    Add immer and some other dependencies

--- .umirc.ts
+++ .umirc.ts
@@ -16,7 +16,9 @@ const config: IConfig = {
         [
             'umi-plugin-react', {
                 antd: true,
-                dva: true,
+                dva: {
+                    immer: true,
+                },
                 dynamicImport: { webpackChunkName: true },
                 title: 'birds-exchanger-ui',
                 dll: false,
