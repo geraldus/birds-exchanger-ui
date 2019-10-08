e2bc0485e8bba837fd317b55a3abbe29c46bf01b
Arthur Sakhievich Fayzrakhmanov <heraldhoi@gmail.com>
2019-10-08 11:49:59 +0500

    Split out breadcrumb as property and render function

--- src/components/bootstrap/default-layout.tsx
+++ src/components/bootstrap/default-layout.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
 import layoutContainer, {
-    BootstrapLayoutProps, pageWrap
+    BootstrapLayoutProps,
+    pageWrap,
+    breadcrumb,
 } from '@/containers/bootstrap/default-layout';
 import { withRouter } from 'react-router';
 import { AppModel, AppConfig } from '@/models/app';
 import { connect } from 'dva';
 
-
 type Props = BootstrapLayoutProps & {
-    appConfig: AppConfig
-}
+    appConfig: AppConfig;
+};
 
 export class BootstrapLayout extends React.Component<Props> {
     render() {
-        const { children, ...props } = this.props
-        const childrenWrap = props.location.pathname === '/'
-            ? children
-            : pageWrap({ children: children })
+        const { children, ...props } = this.props;
+        const { childrenWrap, breadcrumbWrap } =
+            props.location.pathname === '/'
+                ? {
+                      childrenWrap: children,
+                      breadcrumbWrap: null,
+                  }
+                : {
+                      childrenWrap: pageWrap({ children: children }),
+                      breadcrumbWrap: breadcrumb({}),
+                  };
         return layoutContainer({
             children: childrenWrap,
+            breadcrumb: breadcrumbWrap,
             appVersion: this.props.appConfig.app.version,
-            ...props
-        })
+            ...props,
+        });
     }
 }
 
 const mapStateToProps = (state: { app: AppModel }) => {
     return {
-        appConfig: state.app.config
-    }
-}
+        appConfig: state.app.config,
+    };
+};
 
-const connectedLayout = connect(mapStateToProps)(BootstrapLayout)
+const connectedLayout = connect(mapStateToProps)(BootstrapLayout);
 
-export default withRouter(connectedLayout)
+export default withRouter(connectedLayout);
--- src/containers/bootstrap/default-layout.tsx
+++ src/containers/bootstrap/default-layout.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { RouteComponentProps } from 'react-router';
 import navbarLogo from '@/assets/logo/logo-header.png';
 import { NavLink } from 'react-router-dom';
 
-
-export const pageWrap: React.FC = (props) => {
+export const pageWrap: React.FC = props => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12">
-                    {props.children}
+                <div className="col-md-12">{props.children}</div>
+            </div>
+        </div>
+    )
+}
+
+export const breadcrumb: React.FC = () => {
+    return (
+        <div className="container-fluid my-3">
+            <div className="row">
+                <div className="col-11 mx-auto">
+                    <ul className="breadcrumb">
+                        <li className="breadcrumb-item">
+                            <a href="@{fst bc}">#[snd bc]</a>
+                        </li>
+                        <li className="breadcrumb-item active">#[title]</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-8 col-lg-6 mx-auto">
+                    <div className="alert alert-warning" id="message">
+                        #[msg]
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-
 export interface BootstrapLayoutProps extends RouteComponentProps {
-    appVersion?: string
+    appVersion?: string;
+    breadcrumb?: ReactNode;
 }
 
-const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
+const layoutContainer: React.FC<BootstrapLayoutProps> = props => {
     return (
         <div>
             <nav
@@ -39,15 +60,8 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                 >
                     <span className="navbar-toggler-icon" />
                 </button>
-                <NavLink
-                    className="navbar-brand"
-                    to="/"
-                >
-                    <img
-                        src={navbarLogo}
-                        style={{ maxHeight: '20px' }}
-                        alt="OutBirds"
-                    />
+                <NavLink className="navbar-brand" to="/">
+                    <img src={navbarLogo} style={{ maxHeight: '20px' }} alt="OutBirds" />
                 </NavLink>
                 <div
                     id="main-nav-menu"
@@ -64,17 +78,11 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                         <i className="fas fa-wallet align-self-center" />
                         <span className="d-block ml-2"> _[MsgBalance]</span>
                     </span>
-                    <span
-                        className="navbar-text font-weight-bold d-flex d-lg-none wallet-balance-val"
-                    >
+                    <span className="navbar-text font-weight-bold d-flex d-lg-none wallet-balance-val">
                         #[cents2dblT cents]&nbsp;
-                        <span className="font-weight-normal">
-                            #[currSign cur]
-                        </span>
+                        <span className="font-weight-normal">#[currSign cur]</span>
                     </span>
-                    <span className="navbar-text d-block d-lg-none">
-                        #[userName]
-                    </span>
+                    <span className="navbar-text d-block d-lg-none">#[userName]</span>
                     <ul className="navbar-nav d-flex d-lg-none">
                         <li className="nav-item">
                             <a className="nav-link" href="@{route}">
@@ -90,10 +98,7 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                         </li>
                     </ul>
                     <ul className="navbar-nav d-flex">
-                        <li
-                            id="#navWalletDropdownId"
-                            className="nav-item dropdown"
-                        >
+                        <li id="#navWalletDropdownId" className="nav-item dropdown">
                             <a
                                 id="#navWalletDropdownId-toggle"
                                 className="nav-link dropdown-toggle"
@@ -114,16 +119,11 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                                     href="#"
                                 >
                                     #[cents2dblT cents]&nbsp;
-                                    <span className="font-weight-normal">
-                                        #[currSign cur]
-                                    </span>
+                                    <span className="font-weight-normal">#[currSign cur]</span>
                                 </a>
                             </div>
                         </li>
-                        <li
-                            id="#navManageDropdownId"
-                            className="nav-item dropdown"
-                        >
+                        <li id="#navManageDropdownId" className="nav-item dropdown">
                             <a
                                 id="#navManageDropdownId-toggle"
                                 className="nav-link dropdown-toggle"
@@ -139,25 +139,17 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                                 className="dropdown-menu"
                                 aria-labelledby="#navManageDropdownId-toggle"
                             >
-                                <span
-                                    className="navbar-text d-flex px-3 text-uppercase"
-                                >
+                                <span className="navbar-text d-flex px-3 text-uppercase">
                                     <i className="fas fa-pen-nib align-self-center" />
-                                    <span className="d-block ml-2">
-                                        _[MsgEditMenuTitle]
-                                    </span>
+                                    <span className="d-block ml-2">_[MsgEditMenuTitle]</span>
                                 </span>
 
                                 <a className="dropdown-item" href="@{route}">
                                     #[label]
                                 </a>
-                                <span
-                                    className="navbar-text d-flex px-3 text-uppercase"
-                                >
+                                <span className="navbar-text d-flex px-3 text-uppercase">
                                     <i className="fas fa-donate align-self-center" />
-                                    <span className="d-block ml-2">
-                                        _[MsgRequestsMenuTitle]
-                                    </span>
+                                    <span className="d-block ml-2">_[MsgRequestsMenuTitle]</span>
                                 </span>
                                 <a className="dropdown-item" href="@{route}">
                                     #[label]
@@ -167,10 +159,7 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                                 </a>
                             </div>
                         </li>
-                        <li
-                            id="#navUserDropdownId"
-                            className="nav-item dropdown mr-5"
-                        >
+                        <li id="#navUserDropdownId" className="nav-item dropdown mr-5">
                             <a
                                 id="#navUserDropdownId-toggle"
                                 className="nav-link dropdown-toggle"
@@ -194,41 +183,15 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                     </ul>
                 </div>
             </nav>
-            <div className="container-fluid my-3">
-                <div className="row">
-                    <div className="col-11 mx-auto">
-                        <ul className="breadcrumb">
-                            <li className="breadcrumb-item">
-                                <a href="@{fst bc}">#[snd bc]</a>
-                            </li>
-                            <li className="breadcrumb-item active">
-                                #[title]
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-8 col-lg-6 mx-auto">
-                        <div className="alert alert-warning" id="message">
-                            #[msg]
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {props.breadcrumb}
             {props.children}
             <footer className="footer mt-2 pt-3 container-fluid">
                 <div className="row mb-2">
                     <div className="col-sm text-center">
-                        <p className="text-muted mx-2">
-                            _[MsgSupport]: #
-                        </p>
+                        <p className="text-muted mx-2">_[MsgSupport]: #</p>
                     </div>
                     <div className="col-sm text-center">
-                        <a
-                            className="mx-2"
-                            href="mailto:#{supportEmail}"
-                            title="_{MsgSendEmail}"
-                        >
+                        <a className="mx-2" href="mailto:#{supportEmail}" title="_{MsgSendEmail}">
                             <i className="far fa-envelope-open" />
                             #[supportEmail]
                         </a>
@@ -245,17 +208,12 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
                     </div>
                 </div>
                 <div className="row">
-                    <div
-                        className="col pt-3"
-                        style={{ background: '#e9ecef' }}
-                    >
+                    <div className="col pt-3" style={{ background: '#e9ecef' }}>
                         <p className="text-center">
                             _[MsgCurrencyExchangeService] «OutBirds»
                             <small className="text-muted">
                                 v{props.appVersion}
-                                <small className="text-lowercase">
-                                    _[MsgVerPublicBeta]
-                                </small>
+                                <small className="text-lowercase">_[MsgVerPublicBeta]</small>
                             </small>
                         </p>
                     </div>
@@ -263,6 +221,6 @@ const layoutContainer: React.FC<BootstrapLayoutProps> = (props) => {
             </footer>
         </div>
     );
-}
+};
 
-export default layoutContainer
+export default layoutContainer;
